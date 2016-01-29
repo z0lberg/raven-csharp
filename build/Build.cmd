ea59da782bb7d1cda06436269e1d39870f22d0fe
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-01-29 13:11:07 +0100

    Added TODO to build.cmd

--- build/Build.cmd
+++ build/Build.cmd
@@ -1,2 +1,3 @@
 @echo off
+rem TODO: Do something like this to bootstrap Cake: https://github.com/cake-build/cake/blob/develop/build.ps1
 ..\src\packages\Cake.0.8.0\Cake.exe SharpRaven.cake -Target=NuGet-Pack
